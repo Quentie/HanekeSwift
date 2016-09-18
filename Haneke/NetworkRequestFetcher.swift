e46ab8ddf8245f42292aba6df739086ac045ef7f
Kirill Filippov <kfilip@softserveinc.com>
2016-09-18 19:30:53 +0200

    Fixed URL request support

--- Haneke/NetworkRequestFetcher.swift
+++ Haneke/NetworkRequestFetcher.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 /**
-This class extends the original NetworkFetcher from Haneke in order to accept a NSURLRequest rather than only a NSURL
-
-Reason: We need to be able to "mess" with the User-Agent. We can not use the default iOS User-Agent
-Otherwise we will not get the correct images, meant for mobile.
-That is unfortunate IMO, but it's how it works at the moment.
-
-Haneke uses the sharedSession of NSURLSession (NSURLSession.sharedSession()) forthe requests.
-Until iOS 9 we could just add the User-Agent to the additionalHTTPHeeders of the sharedSession,
-this is not the case anymore for iOS >= 9 (is that a bug or intended?)
-
-So now we create the request outside of Haneke, where we define the httpHeaders ourselves
-and then instead of fetching a NSURL, we fetch this NSURLRequest with our custom HTTPHeaders
-*/
+ This class extends the original NetworkFetcher from Haneke in order to accept a NSURLRequest rather than only a NSURL
+ 
+ Reason: We need to be able to "mess" with the User-Agent. We can not use the default iOS User-Agent
+ Otherwise we will not get the correct images, meant for mobile.
+ That is unfortunate IMO, but it's how it works at the moment.
+ 
+ Haneke uses the sharedSession of NSURLSession (NSURLSession.sharedSession()) forthe requests.
+ Until iOS 9 we could just add the User-Agent to the additionalHTTPHeeders of the sharedSession,
+ this is not the case anymore for iOS >= 9 (is that a bug or intended?)
+ 
+ So now we create the request outside of Haneke, where we define the httpHeaders ourselves
+ and then instead of fetching a NSURL, we fetch this NSURLRequest with our custom HTTPHeaders
+ */
 
 public class NetworkRequestFetcher<T : DataConvertible> : NetworkFetcher<T> {
     let request: NSURLRequest
@@ -36,11 +36,12 @@ public class NetworkRequestFetcher<T : DataConvertible> : NetworkFetcher<T> {
     
     public override func fetch(failure fail : @escaping ((Error?) -> ()), success succeed : @escaping (T.Result) -> ()) {
         self.cancelled = false
-        self.task = self.session.dataTask(with: self.URL as URL) {[weak self] (data, response, error) -> Void in
+        
+        self.task = self.session.dataTask(with: self.request as! URLRequest, completionHandler: { [weak self] (data, response, error) -> Void in
             if let strongSelf = self {
                 strongSelf.onReceiveData(data: data , response: response, error: error , failure: fail, success: succeed)
             }
-        }
+            })
         self.task?.resume()
     }
 }
